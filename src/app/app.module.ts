d97a629674942ee551fbbeaa4c984cc6b0dd7891
RezaFarzipour <rezafafa78@gmail.com>
2024-06-03 13:43:25 +0330

    app module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { FilterSearchComponent } from './components/filter-search/filter-search.component';
+import { SearchBoxResultComponent } from './components/search-box-result/search-box-result.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
+  
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterSearchComponent,
+    SearchBoxResultComponent,
+   
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+   
+ 
   ],
   providers: [
     provideClientHydration()
