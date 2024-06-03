03e0135906c3ccaa98e0916fc1d8b5a931269ffb
RezaFarzipour <rezafafa78@gmail.com>
2024-06-03 13:43:42 +0330

    user interface has been maded for typescript

---
+++ src/app/user.ts
@@ -0,0 +1,17 @@
+export interface Users {
+    id:number,
+    name:string,
+    origin_city:string,
+    deathType:string
+    age:number,
+    deathDate:number,
+    fathers_name:string,
+    mothers_name:string,
+    nickname:string,
+    birth_date:number,
+    birth_city:string,
+    death_city:string,
+    burial_city:string,
+    cemetry_name:string,
+    gender:string
+}
